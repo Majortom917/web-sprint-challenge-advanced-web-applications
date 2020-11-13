759d11a45ab8dd5673fabdb1c47f01da63acd78d
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-11-13 08:25:49 -0800

    initial login page and auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,51 @@
 import React from "react";
+import {axiosWithAuth} from '../utils/axiosAuth'
 
 const Login = () => {
+  state = {
+    credentials:{
+      username:'',
+      password:''
+    }
+  }
+  login = e =>{
+    e.preventDefault();
+
+    axiosWithAuth()
+    .post('/api/login', this.state.credentials)
+    .then(res =>{
+      window.localStorage.setItem('token', res.data.payload)
+      this.props.history.push('/protected')
+    })
+    .catch(err => console.log(err))
+  }
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+  render(){
+    return(
+      <div>
+        <form>
+          <input
+            type='text'
+            name='username'
+            placeholder='1'
+            value={}
+            onChange={this.handleChange}
+          />
+
+          <input
+          type='password'
+          name='password'
+          placeholder="2"
+          value={}
+          onChange={this.handleChange}
+
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    )
+  }
+  
 
 export default Login;
