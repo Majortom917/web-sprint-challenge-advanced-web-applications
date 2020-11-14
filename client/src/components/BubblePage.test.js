1836cb003ac276d364526a9347980877465d3864
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-11-13 16:21:18 -0800

    MVP

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -3,5 +3,18 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import App from "../App";
 
 test("Fetches data and renders the bubbles", () => {
-  render(<App/>)
+  render(<App />);
+  const username = screen.getByPlaceholderText('Username');
+  const password = screen.getByPlaceholderText('Password');
+  const loginButton = screen.getByRole('button');
+  const lock = screen.getByText('Protected Page');
+
+  fireEvent.change(username, { target: {value: 'Lambda School'}});
+
+  fireEvent.change(password, { target: {value: "i<3Lambd4"}});
+  fireEvent.click(loginButton);
+  fireEvent.click(lock);
+
+
+  const bubbles = screen.getByText('bubbles');
 });
